cb8535efa70db17f148ee08247f5e5f63fcc3637
parth faladu <parth.fadadu@hotmail.com>
2019-11-12 22:04:20 +0530

    eslint js file add

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#bulb-rules
+    'plugin:vue/recommended',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard',
+    // https://github.com/prettier/eslint-config-prettier
+    'prettier',
+    'prettier/standard',
+    'prettier/vue',
+  ]
+}
